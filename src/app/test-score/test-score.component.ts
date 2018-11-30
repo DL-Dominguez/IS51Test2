0ae3f3c319f2e663b3c40946497c7bbc6a7affcd
DL-Dominguez <Dominguez@mail.fresnostate.edu>
2018-11-29 17:45:41 -0800

    end test

--- src/app/test-score/test-score.component.ts
+++ src/app/test-score/test-score.component.ts
@@ -4,7 +4,15 @@ import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
 
+export interface ITests {
+  id: number;
+  testName: string;
+  pointsPossible: number;
+  pointsRecieved: number;
+  percentage: number;
+  grade: string;
 
+}
 @Component({
   selector: 'app-test-score',
   templateUrl: './test-score.component.html',
@@ -12,7 +20,7 @@ import { ToastService } from '../toast/toast.service';
 })
 export class TestScoreComponent implements OnInit {
 
-  tests: Array<any> = [];
+  tests: Array<ITests> = [];
   constructor(
     private http: Http,
     private activatedRoute: ActivatedRoute,
@@ -21,7 +29,25 @@ export class TestScoreComponent implements OnInit {
   ) { }
 
   async ngOnInit() {
+    this.tests = await this.loadTestsFromJson();
+    Console.log ('this.tests from ngOninit...', this.tests);
 
   }
 
+  async loadTestsFromJson() {
+  const tests = await this.http.get('assets/tests.json').toPromise();
+  return tests.json();
+  }
+
+  addTests() {
+    const test = {
+      id: null,
+      testName: null,
+      pointsPossible: null,
+      pointsRecieved: null,
+      percentage: null,
+      grade: null
+    };
+    this.tests.unshift();
+  }
 }
